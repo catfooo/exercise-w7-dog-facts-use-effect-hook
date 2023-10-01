4bf135cdcebe7a3b41c3b6019deb10961885ef24
So Youn Choi <manghatuhyo@gmail.com>
2023-10-01 10:01:02 +0100

    showing dog fact

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 // Import required  hooks
+import React, { useState, useEffect } from 'react';
 import { DogFact } from "./components/DogFact";
 
 export const App = () => {
   // Hint: Initialize state for storing the dog fact
+  const [factData, setFactData] = useState(null);
   // Hint: Define the API endpoint
+  const API_ENDPOINT = "https://dogapi.dog/api/v2/facts"
   // Hint: Create a function to fetch the dog fact
+  const fetchDogFact = async () => {
+    try {
+      const response = await fetch(API_ENDPOINT)
+      const responseData = await response.json()
+      setFactData(responseData.data[0])
+    } catch (error) {
+      console.error("Failed to fetch dog fact:", error)
+    }
+  }
+
+  // // alternativ: use then/catch
+  // const fetchDogFact = () => {
+  //   fetch(API_ENDPOINT)
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       setFactData(data.data)
+  //     })
+  //     .catch(error => {
+  //       console.error("Failed to fetch dog fact:", error)
+  //     })
+  // } 
+
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+  useEffect(() => {
+    fetchDogFact();
+  }, [])
+
 
   return (
     <div className="App">
-      <DogFact />
+      <DogFact fact={factData} />
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,9 +1,9 @@
 // DogFact Component
-export const DogFact = () => {
+export const DogFact = ({fact}) => {
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop
-
+  const cleanFact = fact?.attributes?.body
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+  return (<div>{cleanFact ? `Dog fact: ${cleanFact}` : "Loading dog fact..."}</div>)
 };
 
 // Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
